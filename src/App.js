32b505b42ef52747a52806d93d1e1cb6fafab1a1
PUSHP JAIN <jainpushp0098@gmail.com>
2023-10-11 21:42:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import OrderList from "./OrderList";
 
-function App() {
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+  const [showTask, setShowTask] = useState(false);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const addItem = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+    setShowTask(true);
+  };
+
+  const deleteItems = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>TODO LIST</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add Items"
+            onChange={itemEvent}
+            value={inputList}
+          />
+          <button onClick={addItem}>+</button>
+          <br />
+          {showTask ? (
+            <div>
+              <div className="task_heading">
+                <h3>Tasks To Do</h3>
+              </div>
+              <ol>
+                {items.map((itemValue, index) => {
+                  return (
+                    <OrderList
+                      key={index}
+                      id={index}
+                      text={itemValue}
+                      onSelect={deleteItems}
+                    />
+                  );
+                })}
+              </ol>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
